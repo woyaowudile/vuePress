1c5480b1162895c372f2788ba9baaed3570d3e4f
meng.tianxiang <meng.tianxiang@rongzer.com>
2021-06-18 11:53:10 +0800

    update

--- docs/.vuepress/components/image-preview.vue
+++ docs/.vuepress/components/image-preview.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="image-preview">
-    <van-image fit="cover"
-            :heght="height || '100%'"
-            :width="width || '100%'"
-            :style="setStyle"
-            :src="url12"
-            @click="clickPreview(url12)" />
+    <van-image
+      fit="cover"
+      :heght="height || '100%'"
+      :width="width || '100%'"
+      :style="setStyle"
+      :src="url12"
+      @click="clickPreview(url12)"
+    />
     <van-image-preview
-            v-model="show"
-            :images="images"
-             @click.native="clickShow"
-            >
-            <template v-slot:index>第{{ index }}页</template>
+      v-model="show"
+      :images="images"
+      @click.native="clickShow"
+    >
+      <template v-slot:index>第{{ index }}页</template>
     </van-image-preview>
     <!-- <Modal title="View Image" v-model="show">
       <img :src="images" v-if="show" style="width: 100%" />
@@ -21,32 +23,32 @@
 
 <script>
 export default {
-    inject: ['env'],
+  inject: ["env"],
   props: {
     imgUrl: {
       type: String,
-      default: ""
+      default: "",
     },
     setStyle: {
-        type: Object,
-        default: () => {}
+      type: Object,
+      default: () => {},
     },
     width: {
       type: String,
-      default: ""
+      default: "",
     },
     height: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
   },
   data() {
     return {
       show: false,
       images: [],
       index: 1,
-      url12: `${this.env?.url}${this.imgUrl}`
-    //   url12: require(`./../statcs/${this.imgUrl}`)
+      url12: `${this.env?.url}${this.imgUrl}`,
+      //   url12: require(`./../statcs/${this.imgUrl}`)
     };
   },
   methods: {
@@ -55,8 +57,8 @@ export default {
       this.images = [url];
     },
     clickShow() {
-      this.show = false
-    }
-  }
+      this.show = false;
+    },
+  },
 };
 </script>
