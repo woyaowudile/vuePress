ee738ffeffea0beddc239f716dce3359a39e7a41
meng.tianxiang <meng.tianxiang@rongzer.com>
2021-07-12 15:45:24 +0800

    配置build

---
+++ build-woyaowudile.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd public
+
+rm ./assets/img/*
+
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m $1
+
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@woyaowudile:woyaowudile/woyaowudile.github.io.git master
+
+# 退出命令
+cd -
